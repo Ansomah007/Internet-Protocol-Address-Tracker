fb118e5e9a2f60bb1a7f3ce1d3d11dde1d930186
Ansomah007 <boahenefaustina7@gmail.com>
2022-10-21 16:12:23 +0000

    update

--- script.js
+++ script.js
@@ -22,3 +22,17 @@ map.zoomTo(11, {
   duration: 2000,
   center: [data.location.lng, data.location.lat]
 });
+}
+getIpAddress();
+
+
+try {
+  document.getElementById('submit').onclick = async function getNewIpAddress() {
+    var url = 'https://geo.ipify.org/api/v2/country,city?';
+    var apikey = 'apiKey=at_XGn4k6pJquxPqauEaqQYMZEoVhkKI&ipAddress=';
+    var input = document.getElementById('mybar').value.toString();
+    // var apiAddress = '102.176.65.232';
+    var apiUrl = url + apikey + input;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data)
